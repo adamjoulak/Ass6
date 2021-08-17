f768d0886f7e6226c03427b586c07aed35a3edb6
Adam Joulak <adam.joulak@se.experis.com>
2021-08-17 15:58:17 +0200

    login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+const Login = () => {
+
+    const [name, setName] = useState("");
+
+    function validateForm() { //validate user input
+        return name.length > 0;
+    };
+
+    function onSubmitClicked(e){ //set name to props
+        console.log(e.target.value)
+    };
+
+    function onNameChanged(e){ //set input username to name state
+        setName(e.target.value.trim());
+    }
+
+    return (
+        <Form>
+             <Form.Group>
+                <Form.Label>Username</Form.Label>
+            </Form.Group>
+            
+            <Form.Group>
+                <Form.Control className="mb-3" autoFocus type="username" placeholder="what yo name?"  value={name} onChange={ onNameChanged }/>
+            </Form.Group>
+            <Button className="btn btn-primary btn-lg" variant="info" type="submit" disabled={ !validateForm() } onClick={ onSubmitClicked }>Submit</Button>
+        </Form>
+    )
+}
+
+export default Login;
