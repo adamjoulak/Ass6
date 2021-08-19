fe61cece32f93b7170bfdd2d5e560c2c0e4fb9ca
Marcus Thornemo Larsson <mumaxs@gmail.com>
2021-08-19 12:37:29 +0200

    fixed whitspace and render with button

--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -1,39 +1,70 @@
 import './Translation.css';
 import { useState } from 'react';
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 const Translation = () => {
-
-    const [sentence, setSentence] = useState({
-        sentence: null
-    })
-
     
+
+    const [sentence, setSentence] = useState([])
+
+     const [imgs, setRenderPics] = useState([]) 
+
+
     const handleTranslation = (event) => { //set input 
-        setSentence({
-            ...sentence,
-            [event.target.id]: event.target.value
-        })
+        setSentence(event.target.value)
     }
-    const renderPic = () => {
-        let str = sentence.sentence.split('');
-        const img = str.map((key, i) => {
-            return <img key={key} src={`./individial_signs/${i}.png`} alt="img" />            
-        });
+
+
+    const onSubmit = (event) => {
+        event.preventDefault() //no reload        
+
+        let str = sentence.split('');
+        console.log(str) 
+        /* str.forEach(x => {
+            if(x !== " "){
+                x += x.toLowerCase() + ".png"
+            }else{
+                str.pop(x)
+            }
+                
+        }); */
+        for (let i = 0; i < str.length; i++) {
+            if(str[i] !== " "){
+                console.log(str[i])
+                str[i] = str[i].toLowerCase() + ".png";
+            }else {
+                delete str[i];
+            }
+            
+        }
+        console.log(str)
+        setRenderPics(str)
+        
     }
 
 
     return (
         /////////////// IMPLEMENT FORM ////////////////////
         <div>
-            <div>
-                <input type="text" id="sentence" placeholder="Enter text" onChange={handleTranslation} />
-                <button onClick={renderPic}>RENDER</button>
-            </div>
-            <div>
-                {sentence.sentence && sentence.sentence.split('').map((i, key) => {
-                    return <img key={key} src={`./individial_signs/${i}.png`} alt="img" />
-                })}                 
-            </div>
+            <Form onSubmit={onSubmit}>
+                <Row>
+                    <Col md={{ span: 3, offset: 3 }}>
+                        <Form.Control placeholder="Enter text" type="search" id="sentence" onChange={handleTranslation} />
+                    </Col>
+                    <Col>
+                        <Button type="Submit" className="mb-2">
+                            Submit
+                        </Button>
+                    </Col>
+                </Row>
+                <Row>  
+                    
+                {imgs && imgs.map((i, key) => {                    
+                   return <img key={key}src={`./individial_signs/${i}`} alt="img" />                    
+                })}
+
+                </Row>
+            </Form>
 
         </div>
     );
