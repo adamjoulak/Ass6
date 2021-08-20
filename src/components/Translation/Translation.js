ebd05c466ca7bb96ce32e4b8e90b2076002f1018
Marcus Thornemo Larsson <mumaxs@gmail.com>
2021-08-20 15:30:29 +0200

    fixes

--- src/components/Translation/Translation.js
+++ src/components/Translation/Translation.js
@@ -1,7 +1,7 @@
 import './Translation.css';
 import { useState } from 'react';
 import { Form, Button, Row, Col } from "react-bootstrap";
-import {getUser} from '../../local-storage/LocalStorage';
+import { getUser } from '../../local-storage/LocalStorage';
 
 const Translation = () => {
 
@@ -16,46 +16,63 @@ const Translation = () => {
         event.preventDefault() //no reload        
 
         let str = sentence.split('');
-        
+
         for (let i = 0; i < str.length; i++) {
-            if(str[i] !== " "){
+            if (str[i] !== " ") {
                 console.log(str[i])
                 str[i] = str[i].toLowerCase() + ".png";
-            }else {
+            } else {
                 delete str[i];
             }
-            
+
         }
         setRenderPics(str)
         saveToDatabase(sentence)
-        
+
     }
 
-    const saveToDatabase = async (sentence) =>{
+    const saveToDatabase = async (sentence) => {
         let name = getUser();
         let userData = null;
-        try{
-            const response = await fetch(`http://localhost:3004/profile/${name.name}`)
+        let putRequest;
+        try {
+            const response = await fetch(`http://localhost:3004/profile?name=${name.name}`)
             userData = await response.json();
-        }catch(error){
+
+        } catch (error) {
             console.log(error);
         }
-        console.log(userData)
+        console.log(userData[0].translations)
 
-        
 
-/*         fetch(`http://localhost:3004/profile/${name.name}`, {
-            method: 'PUT',
-            headers: {"Content-type": "application/json"},
-            body: putRequest
-        })
-        .then(async (response) => {
-            if(!response.ok){
-                const { error = "An unknown error occurred"} = await response.json();
-                throw new Error(error);
+        if (userData[0].translations) {
+            console.log("INTE UNDE")
+            let userTranslations = userData[0].translations;
+            userTranslations.push(sentence)
+            putRequest = {
+                "name": name.name,
+                "translations": userTranslations
             }
-            console.log(putRequest + " added to db!")
-        }) */
+        } else {
+            console.log([sentence])
+            putRequest = {
+                "name": name.name,
+                "translations": [sentence]
+            }
+        }
+        
+        fetch(`http://localhost:3004/profile?name=${name.name}`, {
+            method: 'PUT',
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(putRequest)
+        })
+            .then(async (response) => {
+                if (!response.ok) {
+                    const { error = "An unknown error occurred" } = await response.json();
+                    throw new Error(error);
+                }
+                console.log(putRequest + " added to db!")
+            }) 
     }
 
 
@@ -72,13 +89,13 @@ const Translation = () => {
                         </Button>
                     </Col>
                 </Row>
-                <Row>                     
-                {imgs && imgs.map((i, key) => {                    
-                   return <figure>
-                       <img key={key} src={`./individial_signs/${i}`} alt="img" />  
-                       <figcaption><center>{i.charAt(0)}</center></figcaption>
-                       </figure>                  
-                })}
+                <Row>
+                    {imgs && imgs.map((i, key) => {
+                        return <figure>
+                            <img key={key} src={`./individial_signs/${i}`} alt="img" />
+                            <figcaption><center>{i.charAt(0)}</center></figcaption>
+                        </figure>
+                    })}
 
                 </Row>
             </Form>
